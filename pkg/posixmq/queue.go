6f5255911e7dde9fbd19de16660186cb4e6dbbcb
Taylor Chatfield <tchatfield16@gmail.com>
2022-08-05 12:40:23 -0400

    Add open function

--- pkg/posixmq/queue.go
+++ pkg/posixmq/queue.go
@@ -1,12 +1,17 @@
 package posixmq
 
-import "io"
+import (
+	"io"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
+)
 
 const MessageQueueDefaultMode int = 0644
 
 type MessageQueue struct {
 	io.Closer
-	fd         uintptr
+	fd         int
 	Name       string // Name of the message queue
 	Attributes MessageQueueAttributes
 }
@@ -16,4 +21,31 @@ type MessageQueueAttributes struct {
 	MaxQueueSize    int64 // Max # of messages in queue
 	MaxMessageSize  int64 // Max message size in bytes
 	CurrentMessages int64 // Current # of messages in queue
+}
+
+func Open(name string, flags int64, cfg MessageQueueAttributes) (*MessageQueue, error) {
+	unixName, err := unix.BytePtrFromString(name)
+	if err != nil {
+		return nil, err
+	}
+
+	mqfd, _, errno := unix.Syscall6(
+		unix.SYS_MQ_OPEN,
+		uintptr(unsafe.Pointer(unixName)),
+		uintptr(flags),
+		uintptr(MessageQueueDefaultMode),
+		uintptr(unsafe.Pointer(&cfg)),
+		0, // Last 2 unused
+		0,
+	)
+
+	if errno != 0 {
+		return nil, errno
+	}
+
+	return &MessageQueue{
+		fd:         int(mqfd),
+		Name:       name,
+		Attributes: cfg,
+	}, nil
 }
